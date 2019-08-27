0df0c42b17ba9baf1d597a46d704deff8860a037
Michael McFarland <mcfarland422@McFarlands-MBP.attlocal.net>
2019-08-26 22:14:20 -0400

    first

---
+++ src/firebase/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyApbmRz93WHBOASVUPa4ikiHs0DTGeIbZg",
+  authDomain: "ninja-smoothies-84532.firebaseapp.com",
+  databaseURL: "https://ninja-smoothies-84532.firebaseio.com",
+  projectId: "ninja-smoothies-84532",
+  storageBucket: "ninja-smoothies-84532.appspot.com",
+  messagingSenderId: "823835315575",
+  appId: "1:823835315575:web:57d0b4eb34207785"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
